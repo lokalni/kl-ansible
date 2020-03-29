40ea6fb02f3093caff4b3d03f1c7499afde86d3c
Marek Wajdzik <marek@wajdzik.eu>
2020-03-29 12:22:03 +0200

    Moved from requests to cloudflare lib - fixes pagination issues Changed role name and added ansible tags

--- misc/cloudflare_ns_update.py
+++ misc/cloudflare_ns_update.py
@@ -4,19 +4,25 @@ import re
 import sys
 import time
 import logging
-import requests
+import CloudFlare
 
 # Function to check if should i deal with this dns entry
+
+
 def check_entry(entry):
     logging.info("Checking zone pattern for: {0}".format(entry))
-    return re.match(r"[a-z]+[0-9]+\.(turn|bbb)\.lokalni.pl", entry)
+    return re.match(r"[a-z]+[0-9]+\.bbb\.lokalni.pl", entry)
+
 
 # Setup basic logger
 logging.basicConfig(level=logging.DEBUG)
 
 # Get cloudflare settings from environment
-CF_KEY = os.getenv('CF_KEY') # API Key
-CF_ZONE_ID = os.getenv('CF_ZONE_ID') # Zone identifier
+CF_KEY = os.getenv('CF_KEY')  # API Key
+CF_ZONE_ID = os.getenv('CF_ZONE_ID')  # Zone identifier
+
+# Setup Cloudflare
+cf = CloudFlare.CloudFlare(token=CF_KEY)
 
 if None in [CF_KEY, CF_ZONE_ID]:
     logging.critical("Please set CF_KEY and CF_ZONE_ID")
@@ -29,74 +35,41 @@ entries = {}
 try:
     with open('/var/tmp/cf_entries', 'r') as zone_list:
         for zone_entry in zone_list.readlines():
-            entries[zone_entry.split()[0].strip()] = zone_entry.split()[1].strip()
+            entries[zone_entry.split()[0].strip()] = zone_entry.split()[
+                1].strip()
 except IOError:
     logging.critical("Cannot open zone entries file /var/tmp/cf_entries")
     sys.exit(1)
-    
+
 logging.info("Loaded {0} entries from /var/tmp/cf_entries".format(
-        len(entries)
-    ))
+    len(entries)
+))
 
-# Ask cloudflare for current list of entries
-
-cf_entries = requests.get(
-        "https://api.cloudflare.com/client/v4/zones/{0}/dns_records".format(CF_ZONE_ID), 
-        headers = {
-            "Authorization": "Bearer {0}".format(CF_KEY),
-            "Content-Type": "application/json"
-        }
-).json().get('result', [])
-
-# Dict with cloudflare items that are relevant for us
-cloudflare_items = {}
-
-# Go thru each CF entry
-for entry in entries:
-    for cf_entry in cf_entries:
-        if check_entry(cf_entry['name']) and cf_entry['type']=='A':
-            cloudflare_items[cf_entry['name']] = {
-                    "ip": cf_entry['content'],
-                    "id": cf_entry['id']
-                }
 # Add missing items
 for entry in entries:
-    if not entry in cloudflare_items:
-        # Add CF entry
-        requests.post(
-                "https://api.cloudflare.com/client/v4/zones/{0}/dns_records".format(CF_ZONE_ID),
-                headers = {
-                    "Authorization": "Bearer {0}".format(CF_KEY),
-                    "Content-Type": "application/json"
-                },
-                json = {
-                    "type": "A",
-                    "proxied": False,
-                    "ttl": 1,
-                    "content": entries[entry],
-                    "name": entry,
-                    "priority": 10
-                }
-        )
-        logging.info("Added {} to CF".format(entry))
+    dns_record = cf.zones.dns_records.get(
+        CF_ZONE_ID, params={'name': entry, 'match': 'all', 'type': 'A'})
+    # No entry - add
+    if len(dns_record) == 0:
+        cf.zones.dns_records.post(CF_ZONE_ID, data={
+            'name': entry,
+            'type': 'A',
+            'content': entries[entry],
+            'proxied': False
+        })
+        logging.info("Created: {0}".format(entry))
     else:
-        if cloudflare_items[entry]['ip'] != entries[entry]:
-            # We need to update this entry
-            requests.put(
-                    "https://api.cloudflare.com/client/v4/zones/{0}/dns_records/{1}".format(CF_ZONE_ID, cloudflare_items[entry]['id']),
-                    headers = {
-                        "Authorization": "Bearer {0}".format(CF_KEY),
-                        "Content-Type": "application/json"
-                    },
-                    json = {
-                        "type": "A",
-                        "proxied": False,
-                        "ttl": 1,
-                        "content": entries[entry],
-                        "name": entry,
-                    }
-            )
-            logging.info("Updated {} CF entry".format(entry))
-
+        # Entry in place - update if needed
+        if dns_record[0]['content'] != entries[entry]:
+            cf.zones.dns_records.put(CF_ZONE_ID, dns_record[0]['id'], data={
+                'name': entry,
+                'type': 'A',
+                'content': entries[entry],
+                'proxied': False
+            })
+            logging.info("Updated: {0}".format(entry))
+        else:
+            logging.info("No changes: {0}".format(entry))
 # Now we are going to sleep for 30s, next ansible step must wait for CF propagation which takes around 15sec
+logging.info("Sleeping for 30s".format(entry))
 time.sleep(30.0)
